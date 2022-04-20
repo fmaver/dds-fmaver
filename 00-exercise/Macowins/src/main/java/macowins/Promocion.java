141b26243c8252fe04306378dc1fb3174cefd763
fmaver <fmaver@frba.utn.edu.ar>
2022-04-20 16:52:28 -0300

    Acomodo el formato

--- 00-exercise/Macowins/src/main/java/macowins/Promocion.java
+++ 00-exercise/Macowins/src/main/java/macowins/Promocion.java
@@ -1,13 +1,9 @@
 package macowins;
-
 class Promocion implements EstadoPrenda {
-
     double descuento;
-
     public Promocion(double descuento) {
         this.descuento = descuento;
     }
-
     @Override
     public double precioFinal(double precioOriginal) {
         return precioOriginal - descuento;
